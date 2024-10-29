af76d3572d88063f1c55bc163f79509402199777
LincolnCB <lcravenbrightman@gmail.com>
2024-10-29 17:27:14 -0400

    Added pullup to UART1 block design, added rev d device tree source files to try and enable uart

--- projects/example_uart_connection/block_design.tcl
+++ projects/example_uart_connection/block_design.tcl
@@ -4,6 +4,7 @@
 # 1: Use board preset
 # - Disable unneccessary M_AXI_GP0 and S_AXI_ACP interface
 # - Enable and assign UART1
+# - Enable pull-up resistors for the UART1 pins
 # No connections necessary, this is a PS-focused project
 init_ps ps_0 1 {
   PCW_USE_M_AXI_GP0 0
@@ -11,4 +12,6 @@ init_ps ps_0 1 {
   PCW_USE_DEFAULT_ACP_USER_VAL 0
   PCW_UART1_PERIPHERAL_ENABLE 1
   PCW_UART1_UART1_IO {MIO 36 .. 37}
+  PCW_MIO_36_PULLUP enabled
+  PCW_MIO_37_PULLUP enabled
 } {}
