63e17a516e590945eaaa666ce36f0eba8203c27d
LincolnCB-virt <lcravenbrightman@gmail.com>
2025-05-13 20:23:10 -0400

    Updated block design for ex05_multi_dma

--- projects/ex05_multi_dma/block_design.tcl
+++ projects/ex05_multi_dma/block_design.tcl
@@ -29,52 +29,75 @@ cell xilinx.com:ip:smartconnect:1.0 axi_ps_periph_intercon {
   NUM_MI 1
 } {
   aclk ps/FCLK_CLK0
-  S00_AXI /ps/M_AXI_GP0
   aresetn ps_rst/peripheral_aresetn
+  S00_AXI ps/M_AXI_GP0
 }
 # AXI memory interconnect
 cell xilinx.com:ip:smartconnect:1.0 axi_mem_intercon {
-  NUM_SI 2
+  NUM_SI 3
   NUM_MI 1
 } {
   aclk ps/FCLK_CLK0
-  M00_AXI ps/S_AXI_HP0
   aresetn ps_rst/peripheral_aresetn
+  M00_AXI ps/S_AXI_HP0
 }
 
-# Create the AXI DMA core
-cell xilinx.com:ip:axi_dma:7.1 axi_dma {
-  c_enable_multi_channel 0
-  c_include_sg 0
-  c_sg_include_stscntrl_strm 0
+# Create the AXI Multi-channel DMA core
+cell xilinx.com:ip:axi_mcdma:1.2 axi_mcdma {
+  c_num_mm2s_channels 8
+  c_num_s2mm_channels 8
 } {
+  s_axi_aclk ps/FCLK_CLK0
   s_axi_lite_aclk ps/FCLK_CLK0
-  m_axi_mm2s_aclk ps/FCLK_CLK0
-  m_axi_s2mm_aclk ps/FCLK_CLK0
   axi_resetn ps_rst/peripheral_aresetn
   S_AXI_LITE axi_ps_periph_intercon/M00_AXI
   M_AXI_MM2S axi_mem_intercon/S00_AXI
   M_AXI_S2MM axi_mem_intercon/S01_AXI
+  M_AXI_SG axi_mem_intercon/S02_AXI
 }
 ## Set addresses (these match the automation, but is better to have explicit)
 # Set the DMA control address
-addr 0x40400000 64k axi_dma/S_AXI_LITE ps/M_AXI_GP0
+addr 0x40400000 64k axi_mcdma/S_AXI_LITE ps/M_AXI_GP0
 # Set the AXI_HP memory addresses
-addr 0x00000000 1G ps/S_AXI_HP0 axi_dma/M_AXI_MM2S
-addr 0x00000000 1G ps/S_AXI_HP0 axi_dma/M_AXI_S2MM
+addr 0x00000000 1G ps/S_AXI_HP0 axi_mcdma/M_AXI_MM2S
+addr 0x00000000 1G ps/S_AXI_HP0 axi_mcdma/M_AXI_S2MM
+addr 0x00000000 1G ps/S_AXI_HP0 axi_mcdma/M_AXI_SG
 
 # Interrupt concat (necessary)
 cell xilinx.com:ip:xlconcat:2.1 intr_concat {
-  NUM_PORTS 2
+  NUM_PORTS 16
 } {
-  In0 axi_dma/mm2s_introut
-  In1 axi_dma/s2mm_introut
+  In0  axi_mcdma/mm2s_ch1_introut
+  In1  axi_mcdma/s2mm_ch1_introut
+  In2  axi_mcdma/mm2s_ch2_introut
+  In3  axi_mcdma/s2mm_ch2_introut
+  In4  axi_mcdma/mm2s_ch3_introut
+  In5  axi_mcdma/s2mm_ch3_introut
+  In6  axi_mcdma/mm2s_ch4_introut
+  In7  axi_mcdma/s2mm_ch4_introut
+  In8  axi_mcdma/mm2s_ch5_introut
+  In9  axi_mcdma/s2mm_ch5_introut
+  In10 axi_mcdma/mm2s_ch6_introut
+  In11 axi_mcdma/s2mm_ch6_introut
+  In12 axi_mcdma/mm2s_ch7_introut
+  In13 axi_mcdma/s2mm_ch7_introut
+  In14 axi_mcdma/mm2s_ch8_introut
+  In15 axi_mcdma/s2mm_ch8_introut
   dout ps/IRQ_F2P
 }
-# AXIS loopback
-cell xilinx.com:ip:axis_data_fifo:2.0 axis_loopback {} {
-  S_AXIS axi_dma/M_AXIS_MM2S
-  M_AXIS axi_dma/S_AXIS_S2MM
-  s_axis_aresetn ps_rst/peripheral_aresetn
-  s_axis_aclk ps/FCLK_CLK0
+# AXIS Broadcaster
+cell xilinx.com:ip:axis_broadcaster:1.1 axis_broadcaster {
+  NUM_MI 8
+} {
+  aclk ps/FCLK_CLK0
+  aresetn ps_rst/peripheral_aresetn
+  S_AXIS axi_mcdma/M_AXIS_MM2S
+}
+# AXIS Combiner
+cell xilinx.com:ip:axis_combiner:1.1 axis_combiner {
+  NUM_SI 8
+} {
+  aclk ps/FCLK_CLK0
+  aresetn ps_rst/peripheral_aresetn
+  M_AXIS axi_mcdma/S_AXIS_S2MM
 }
